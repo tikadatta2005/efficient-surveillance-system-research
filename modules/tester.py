a7ce7bc99b7266c19675f0e1ee07b37290fab493
Deepsu Gautam <tikadatta2005@gmail.com>
2026-07-15 03:19:04 +0545

    update/ tester update, experiment 3 is ready to use

--- modules/tester.py
+++ modules/tester.py
@@ -7,36 +7,17 @@ from modules.evaluator import evaluator
 def test(model, save_path, data, criterion, device):
     # set model to device
     model.to(device)
-    # check points file
-    model_files = [
-            f for f in os.listdir(f"{save_path}/models")
-            if f.endswith(".pt") and "epoch_" in f
-        ]
-    
-    # metrics
-    metrics = []
-    
-    # loop through each file
-    for epoch, file in enumerate(model_files):
-        # deep copy the model
-        test_model = copy.deepcopy(model)
-        # load checkpoint
-        checkpoint = torch.load(
-            os.path.join(save_path, file),
-            map_location=device
-            )
-        # load state dict to model
-        test_model.load_state_dict(checkpoint)
-        # evaluate model
-        test_loss, test_preds, test_targets = evaluator(model, data, criterion, device)
-        # get metric 
-        metric = calculate_metrics(test_targets, test_preds, prefix="test_")
-        metric["test_loss"] = test_loss
-        metric["epoch"] = epoch+1
-        # append to the metrics
-        metrics.append(metric)
-    
-    # convert to pandas
-    metrics = pd.DataFrame(metrics)
-    metrics.to_csv(os.path.join(f"{save_path}/test_metrics.csv"), index=False)
-    return metrics
+    # load checkpoint 
+    checkpoint = torch.load(save_path, map_location=device)
+    # make a copy of the model
+    test_model = copy.deepcopy(model)
+    # load state dict
+    test_model.load_state_dict(checkpoint)
+    # evaluate model  
+    test_loss, test_preds, test_targets = evaluator(model, data, criterion, device)
+    # get metric 
+    metric = calculate_metrics(test_targets, test_preds, prefix="test_")
+    # show loss
+    metric["test_loss"] = test_loss
+    # return metric
+    return metric
